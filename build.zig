bf3158050277628148f8d63f54677fba5c9560cd
Tom <0x546f6d@protonmail.com>
2025-02-01 14:32:40 +0300

    basic example build with option

--- build.zig
+++ build.zig
@@ -14,12 +14,15 @@ pub fn build(b: *std.Build) void {
     });
     brotli_mod.linkLibrary(brotli_c.artifact("brotli_lib"));
 
-    const exe = b.addExecutable(.{
-        .name = "basic",
-        .root_source_file = b.path("examples/basic.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    exe.root_module.addImport("brotli", brotli_mod);
-    b.installArtifact(exe);
+    const build_basic = b.option(bool, "build-basic", "Build basic example executable (default:false)") orelse false;
+    if (build_basic) {
+        const exe = b.addExecutable(.{
+            .name = "basic",
+            .root_source_file = b.path("examples/basic.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.root_module.addImport("brotli", brotli_mod);
+        b.installArtifact(exe);
+    }
 }
